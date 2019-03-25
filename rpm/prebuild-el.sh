ba649d2aeaea8c7dff9bfceb186da215c6ff7b9b
Alexander Turenko <alexander.turenko@tarantool.org>
2019-03-25 22:34:35 +0300

    Update JRE download URL
    The old one gives 404 Not found.

--- rpm/prebuild-el.sh
+++ rpm/prebuild-el.sh
@@ -1,6 +1,6 @@
 echo "Installing JRE"
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
-"http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jre-8u151-linux-x64.rpm" -O /tmp/jre.rpm
+"https://download.oracle.com/otn-pub/java/jdk/8u202-b08/1961070e4c9b4e26a04e7f5a083f551e/jdk-8u202-linux-x64.rpm" -O /tmp/jre.rpm
 sudo yum localinstall -y /tmp/jre.rpm
 
 echo "Installing cloudera repo"
