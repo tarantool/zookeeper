6264b5b754bfcd445fc9758e320635b7146c14b2
Alexander Turenko <alexander.turenko@tarantool.org>
2019-03-25 20:14:12 +0300

    Let packpack enable tarantool repository
    We have no tarantool packages for Ubuntu Bionic in 1_7 repository.
    
    Set PACKAGECLOUD_USER=tarantool and PACKAGECLOUD_REPO=1_10 in Travis-CI
    variables. Now packpack will enable this repository and tarantool-1.10
    will be installed as a dependency.

--- rpm/prebuild-el.sh
+++ rpm/prebuild-el.sh
@@ -1,29 +1,3 @@
-echo "Installing Tarantool 1.7"
-
-sudo yum clean all
-sudo yum -y install http://dl.fedoraproject.org/pub/epel/epel-release-latest-${DIST}.noarch.rpm
-sudo sed 's/enabled=.*/enabled=1/g' -i /etc/yum.repos.d/epel.repo
-sudo rm -f /etc/yum.repos.d/*tarantool*.repo
-sudo tee /etc/yum.repos.d/tarantool_1_7.repo <<- EOF
-[tarantool_1_7]
-name=EnterpriseLinux-${DIST} - Tarantool
-baseurl=http://download.tarantool.org/tarantool/1.7/el/${DIST}/x86_64/
-gpgkey=http://download.tarantool.org/tarantool/1.7/gpgkey
-repo_gpgcheck=1
-gpgcheck=0
-enabled=1
-
-[tarantool_1_7-source]
-name=EnterpriseLinux-${DIST} - Tarantool Sources
-baseurl=http://download.tarantool.org/tarantool/1.7/el/${DIST}/SRPMS
-gpgkey=http://download.tarantool.org/tarantool/1.7/gpgkey
-repo_gpgcheck=1
-gpgcheck=0
-EOF
-
-sudo yum makecache -y --disablerepo='*' --enablerepo='tarantool_1_7' --enablerepo='epel'
-sudo yum -y install tarantool
-
 echo "Installing JRE"
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
 "http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jre-8u151-linux-x64.rpm" -O /tmp/jre.rpm
